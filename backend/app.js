1bb0a222186f0638cc26dcbbf232893d213d249b
Ekaterina Sokolova <sokoloffkate@gmail.com>
2023-04-06 18:54:44 +1000

    change connect mongoose in app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 app.use(express.json());
