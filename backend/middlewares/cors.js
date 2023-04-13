4897f71d74e9b3cf31454c7fd4248f90782722b7
Ekaterina Sokolova <sokoloffkate@gmail.com>
2023-04-14 01:58:37 +1000

    modify cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,7 +11,7 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
-  const { method } = req;
+  /*const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
 
@@ -19,7 +19,7 @@ module.exports = (req, res, next) => {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
-  }
+  }*/
 
   return next();
 };
